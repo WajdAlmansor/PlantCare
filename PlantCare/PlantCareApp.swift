94a9702181f51a6da1be59a994567e206bc6cb93
WajdButterfly <wajd2828@gmail.com>
2024-10-20 16:24:39 +0300

    Initial Commit

---
+++ PlantCare/PlantCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantCareApp.swift
+//  PlantCare
+//
+//  Created by Wajd on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
